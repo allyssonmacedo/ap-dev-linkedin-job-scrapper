3a51a5bf70a27a39b45f969a9bd7e18b445e92d1
Allysson Macedo <allyssonvsmacedo@gmail.com>
2023-11-12 23:44:11 -0300

    fix getJobAttr and create SearchParameters

--- main.py
+++ main.py
@@ -4,19 +4,42 @@ import time
 import json
 import requests
 import bs4
+import connectmongo
 from bs4 import BeautifulSoup as bs
 from functions import JobScrapping
+from urllib.parse import unquote
+import datetime
+
 
 search = "https://www.linkedin.com/jobs/search/?currentJobId=3743834990&keywords=data%20analyst&origin=SWITCH_SEARCH_VERTICAL"
 
-jobs_url = JobScrapping().getJobsURL(search)
+jobs_url = JobScrapping().getJobsURL(search, max_jobs=20)
 
 soup_jobs = JobScrapping().getJobSoup(jobs_url)
 
-print(JobScrapping().getJobAttr(soup_jobs[0]))
-
 jobs_posts = []
-for job in range(len(soup_jobs)):
-    jobs_posts.append(JobScrapping().getJobAttr(soup_jobs[job]))
+for job in soup_jobs:
+    post = JobScrapping().getJobPost(job)
+    jobs_posts.append(post)
 
-### Salvar os dados no banco.
+
+### Save in Mongo DB
+client = connectmongo.connectServer()
+
+db_connection = client['jobs-in']
+
+collection = db_connection.get_collection('posts')
+
+data = collection.insert_many(jobs_posts)
+
+### Save Query Parameters in Search
+search_query = {
+    "author": 'crawler',
+    "searchedLink": search,
+    "searchedJobs": jobs_url,
+    "searchedParameters": JobScrapping().getSearchParameters(search),
+    "searchedTime": datetime.datetime.now(tz=datetime.timezone.utc),
+}
+
+searches = db_connection.get_collection('searches')
+searched = searches.insert_one(search_query)
